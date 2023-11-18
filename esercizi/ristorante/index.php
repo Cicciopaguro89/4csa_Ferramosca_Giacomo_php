cbc575ee46c58ea4f5f2726a03d07b0e4cd2492f
Cicciopaguro89 <146830785+Cicciopaguro89@users.noreply.github.com>
2023-11-18 19:57:46 +0000

    1

--- esercizi/ristorante/index.php
+++ esercizi/ristorante/index.php
@@ -1,76 +1,44 @@
 <!DOCTYPE html>
-<style> body{
-            background-color: #60c8e7;
-            margin: 0px;
-            font-family: Verdana, Arial, Helvetica, sans-serif; 
-            width: 21cm;
-        }
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Menu del Ristorante</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container mt-5">
+  <h1 class="mb-4">Menu del Ristorante</h1>
+  <?php
+    $menu = array("Rigatoni alla carbonara"=>8.50, "Pizza margherita"=>6.99, "Tagliatelle al ragù"=>11.00, "Caffè"=>1.50, "Tiramisù"=>4.50);
+    echo '<div class="list-group">';
+    foreach ($menu as $piatto=>$c) {
+      echo '<a href="#" class="list-group-item list-group-item-action">' . $piatto . "<br>" . $c . '</a>';
      
-        #cornice {
-            background-color: #ffffff;
-            margin: 25px;
-            padding: 10px;
-            border: 2px solid #000000;
-            color: #000000;
-        }
+      if ($piatto === "Rigatoni alla carbonara")
+      {
+        echo "<img src= 'https://www.sgambaro.it/wp-content/uploads/2022/04/carbonara_1600x2000.jpg'";
+      }elseif ($piatto === "Pizza margherita")
+      {
+        echo "<img src= 'https://it.ooni.com/cdn/shop/articles/Margherita-9920.jpg?crop=center&height=800&v=1644590028&width=800'";
+      }elseif ($piatto === "Tagliatelle al ragù")
+      {
+        echo "<img src= 'https://media-assets.lacucinaitaliana.it/photos/61fb130ae334e788eab040b9/1:1/w_800,h_800,c_limit/Tagliatelle-con-rag%C3%B9-alla-bolognese.jpg'";
+      }elseif ($piatto === "Caffè")
+      {
+        echo "<img src= 'https://www.caffeborbone.com/dw/image/v2/BHCB_PRD/on/demandware.static/-/Library-Sites-CaffeBorboneSharedLibrary/default/dw014eba09/split_banner/caffe-espresso-quali-sono-le-sue-origini_split.jpg?sw=1090&sh=728'";
+      }elseif ($piatto === "Tiramisù")
+      {
+        echo "<img src= 'https://www.cucchiaio.it/content/dam/cucchiaio/it/ricette/2023/08/tiramisu-cremoso/tiramisu-cremoso-finale.jpg'" ;
+      }
+    }
+    echo '</div>';
+  ?>
 
-        .titolo {
-            width: 70%;
-            border-top-width: 0px;
-            border-right-width: 0px;
-            border-bottom-width: 1px;
-            border-left-width: 0px;
-            border-top-style: none;
-            border-right-style: none;
-            border-bottom-style: double;
-            border-left-style: none;
-            border-top-color: #FFFFFF;
-            border-right-color: #FFFFFF;
-            border-bottom-color: #000000;
-            border-left-color: #FFFFFF;
-            margin: 20px 0px 10px;
-            padding: 0px;
-        }
-
-        h1 {
-            padding: 0px;
-            font-size: 14pt;
-            float: right;
-        }
-
-        h2 {
-            padding: 0px;
-            margin: 0px;
-            font-size: 1em;
-        }
-
-        .content {
-            font-size: 8pt;
-        }
-        li {
-            padding: 0px;
-            font-size: 8pt;
-        }
-        p {
-            padding: 0px;
-            font-size: 8pt;;
-        }
-    </style>
-<h1>Menù ristorante da Gigi</h1><br><br><br>
-<table>
-    <td><tr> </tr></td>
-
-<?php
-$taglio=["tagliolini al ragu ","10$"];
-$ling=["linguine al pesto ","11$"];
-$spago=["spaghetti alle vongole ","15$"];
-$canne=["cannelloni alla carbonara ","12$"];
-$menu=[$taglio,$ling,$spago,$canne];
-
-foreach($menu as $piatto)
-{
-    echo "$piatto[0] ";
-    echo "$piatto[1] <br>";
-}
-?>
-</table>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
