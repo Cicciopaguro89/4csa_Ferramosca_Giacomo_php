ba123e7490700c215e9a6bf5520e425d616b8f89
Cicciopaguro89 <146830785+Cicciopaguro89@users.noreply.github.com>
2023-12-21 14:13:18 +0000

    2

--- esercizi/verifica form/tabella.php
+++ esercizi/verifica form/tabella.php
@@ -4,15 +4,12 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <title>Tabella creata da AxB</title>
 </head>
-
 <body>
 <?php
     $testo=$_GET ['testo'];
     $righe=$_GET ['righe'];
     $colonne=$_GET ['colonne'];
-   
     echo '<table class="table table-striped">';
-
     for ($i = 1;$i <= $righe;$i++)
     {
         echo "<tr>";
@@ -22,9 +19,6 @@
         }
         echo "</tr>";
     }
-
-
-
 ?>
 </body>
 </html>
